d8a027cf9d8849f557e3640a3cde816a3656ec49
MattDahEpic <mattdahepic@gmail.com>
2025-08-09 06:31:28 +0000

    asyncify

--- custom_components/midas/api.py
+++ custom_components/midas/api.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from california_midasapi import Midas
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 if TYPE_CHECKING:
     from california_midasapi.ratelist import RateInfo
@@ -22,12 +23,12 @@ class IntegrationMidasApiClient:
     ) -> None:
         """Midas API Client."""
         self._hass = hass
-        self._midas = Midas(username, password)
+        self._midas = Midas(async_get_clientsession(hass), username, password)
 
     async def async_get_rate_data(self, rate_id: str) -> RateInfo:
         """Get data from the API."""
-        return await self._hass.async_add_executor_job(self._midas.GetRateInfo, rate_id)
+        return await self._midas.GetRateInfo(rate_id)
 
     async def async_test_credentials(self) -> None:
         """Check for validity of the set credentials. Throws if invalid."""
-        await self._hass.async_add_executor_job(self._midas.test_credentials)
+        await self._midas.test_credentials()
