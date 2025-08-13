d8f862879e8ccb90b191aebe4cd4d81acb15f1b3
MattDahEpic <mattdahepic@gmail.com>
2025-08-13 02:15:44 +0000

    create issue when no active tariffs for rate (fixes #71)

--- custom_components/midas/coordinator.py
+++ custom_components/midas/coordinator.py
@@ -8,6 +8,7 @@ from typing import TYPE_CHECKING
 from california_midasapi.exception import MidasAuthenticationException, MidasException
 from california_midasapi.types import RateInfo
 from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.helpers import issue_registry
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import DOMAIN, LOGGER
@@ -55,8 +56,24 @@ class MidasDataUpdateCoordinator(DataUpdateCoordinator[dict[str, RateInfo]]):
                 tariffs = data[rid].GetCurrentTariffs()
                 # Check if there are any tariffs and issue error if not
                 if len(tariffs) == 0:
-                    LOGGER.error(
-                        f"Rate ID {rid} has no active tariffs! This may mean the utility has changed Rate IDs or has simply stopped submitting data to MIDAS. Check your latest bill for a new Rate ID. If this persists, please reach out to your utility and tell them the Rate ID on your bill is not returning any data for your smart home system to use."  # noqa: E501
+                    issue_registry.async_create_issue(
+                        self.hass,
+                        DOMAIN,
+                        f"no_tarrifs_{rid.lower()}",
+                        is_fixable=False,
+                        is_persistent=False,
+                        severity=issue_registry.IssueSeverity.ERROR,
+                        translation_key="no_tariffs",
+                        translation_placeholders={"rid": rid},
+                    )
+                    LOGGER.debug(
+                        f"Rate ID {rid} has no active tariffs! An issue was created."
+                    )
+                else:
+                    issue_registry.async_delete_issue(
+                        self.hass,
+                        DOMAIN,
+                        f"no_tarrifs_{rid.lower()}",
                     )
         except MidasAuthenticationException as exception:
             raise ConfigEntryAuthFailed(exception) from exception
