508bde626487bcdc96cdb46344b89d9796a5ebd2
m <matt@mattdahepic.com>
2025-11-14 03:40:41 +0000

    more config flow tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,17 @@
 """Fixtures for testing."""
 
+from collections.abc import Generator
+from unittest.mock import AsyncMock, patch
+
 import pytest
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.midas.const import (
+    CONF_PASSWORD,
+    CONF_RATEIDS,
+    CONF_USERNAME,
+    DOMAIN,
+)
 
 pytest_plugins = ["aiohttp.pytest_plugin"]  # makes AiohttpClientMocker work
 
@@ -9,3 +20,26 @@ pytest_plugins = ["aiohttp.pytest_plugin"]  # makes AiohttpClientMocker work
 def auto_enable_custom_integrations(enable_custom_integrations):
     """Enable actually testing with the custom integration."""
     yield
+
+
+@pytest.fixture
+def mock_setup_entry() -> Generator[AsyncMock]:
+    """Override async_setup_entry."""
+    with patch(
+        "custom_components.midas.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        yield mock_setup_entry
+
+
+@pytest.fixture
+def mock_config_entry() -> MockConfigEntry:
+    """Return the mocked config entry."""
+    return MockConfigEntry(
+        title="MIDAS Account: test",
+        domain=DOMAIN,
+        data={
+            CONF_USERNAME: "test",
+            CONF_PASSWORD: "test",
+            CONF_RATEIDS: ["TEST-TEST-TEST-TEST"],
+        },
+    )
--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -5,17 +5,20 @@
 # ruff: noqa: S101
 
 from http import HTTPStatus
+from unittest.mock import AsyncMock
 
 from aiohttp import ServerTimeoutError
-from homeassistant.config_entries import SOURCE_USER
+from homeassistant.config_entries import SOURCE_RECONFIGURE, SOURCE_USER
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
 
 from custom_components.midas.const import (
     CONF_EMAIL,
     CONF_NAME,
     CONF_PASSWORD,
+    CONF_RATEIDS,
     CONF_USERNAME,
     DOMAIN,
 )
@@ -271,3 +274,229 @@ async def test_config_existing_account_valid_credentials(
     )
     assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "options"
+
+
+async def test_config_rateids_no_rateids(
+    hass: HomeAssistant,
+    aioclient_mock: AiohttpClientMocker,
+) -> None:
+    """Test that configuring requires at least one RIN."""
+    # have account? menu
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.MENU
+    assert result["step_id"] == SOURCE_USER
+    # login
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={"next_step_id": "auth"}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "auth"
+    # account credentials
+    aioclient_mock.get(
+        "https://midasapi.energy.ca.gov/api/token",
+        status=HTTPStatus.OK,
+        text="Token issued and will expire in 10 minutes.",
+        headers={"Content-Type": "text/plain; charset=utf-8", "Token": "fake_token"},
+    )
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_USERNAME: "test",
+            CONF_PASSWORD: "test",
+        },
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "options"
+    # test empty rateids
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_RATEIDS: [],
+        },
+    )
+    assert result["errors"].get("base") == "rateids_missing"
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "options"
+
+
+async def test_config_rateids_invalid_rateids(
+    hass: HomeAssistant,
+    aioclient_mock: AiohttpClientMocker,
+) -> None:
+    """Test that providing invalid rateids gives an error."""
+    # have account? menu
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.MENU
+    assert result["step_id"] == SOURCE_USER
+    # login
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={"next_step_id": "auth"}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "auth"
+    # account credentials
+    aioclient_mock.get(
+        "https://midasapi.energy.ca.gov/api/token",
+        status=HTTPStatus.OK,
+        text="Token issued and will expire in 10 minutes.",
+        headers={"Content-Type": "text/plain; charset=utf-8", "Token": "fake_token"},
+    )
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_USERNAME: "test",
+            CONF_PASSWORD: "test",
+        },
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "options"
+    # test empty rateids
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_RATEIDS: ["TEST-WRONG-FORMAT"],
+        },
+    )
+    assert result["errors"].get("base") == "rateid_invalid"
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "options"
+
+
+async def test_config_rateids_valid_rateids(
+    hass: HomeAssistant,
+    aioclient_mock: AiohttpClientMocker,
+    mock_setup_entry: AsyncMock,
+) -> None:
+    """Test that providing valid rateids completes the configuration."""
+    # have account? menu
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.MENU
+    assert result["step_id"] == SOURCE_USER
+    # login
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={"next_step_id": "auth"}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "auth"
+    # account credentials
+    aioclient_mock.get(
+        "https://midasapi.energy.ca.gov/api/token",
+        status=HTTPStatus.OK,
+        text="Token issued and will expire in 10 minutes.",
+        headers={"Content-Type": "text/plain; charset=utf-8", "Token": "fake_token"},
+    )
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_USERNAME: "test",
+            CONF_PASSWORD: "test",
+        },
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "options"
+    # test empty rateids
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_RATEIDS: ["TEST-TEST-TEST-TEST"],
+        },
+    )
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["title"] == "MIDAS Account: test"
+    assert result["data"].get(CONF_USERNAME) == "test"
+    assert result["data"].get(CONF_PASSWORD) == "test"
+    assert result["data"].get(CONF_RATEIDS) == ["TEST-TEST-TEST-TEST"]
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_config_reconfigure_no_rateids(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test that reconfiguring still requires at least one RIN."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+
+    # start reconfigure flow
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_RECONFIGURE, "entry_id": mock_config_entry.entry_id},
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == SOURCE_RECONFIGURE
+    # test empty rateids
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_RATEIDS: [],
+        },
+    )
+    assert result["errors"].get("base") == "rateids_missing"
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == SOURCE_RECONFIGURE
+
+
+async def test_config_reconfigure_invalid_rateids(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test that reconfiguring with invalid rate ids presents an error."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+
+    # start reconfigure flow
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_RECONFIGURE, "entry_id": mock_config_entry.entry_id},
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == SOURCE_RECONFIGURE
+    # test empty rateids
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_RATEIDS: ["TEST-WRONG-FORMAT"],
+        },
+    )
+    assert result["errors"].get("base") == "rateid_invalid"
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == SOURCE_RECONFIGURE
+
+
+async def test_config_reconfigure_valid_rateids(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test that reconfiguring with valid rateids works."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+
+    # start reconfigure flow
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_RECONFIGURE, "entry_id": mock_config_entry.entry_id},
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == SOURCE_RECONFIGURE
+    # test empty rateids
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_RATEIDS: ["TEST-TEST-TEST-NEW1"],
+        },
+    )
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+
+    updated_entry = hass.config_entries.async_get_entry(mock_config_entry.entry_id)
+    assert updated_entry.data[CONF_USERNAME] == "test"
+    assert updated_entry.data[CONF_PASSWORD] == "test"
+    assert updated_entry.data[CONF_RATEIDS] == ["TEST-TEST-TEST-NEW1"]
+
+
